2cc071ead1051ad1b5bb329fd4ebe757bf1a2b54
김석환 <ksh930610@gmail.com>
2017-08-14 20:31:44 +0900

    pop-up

--- RokLee.java
+++ RokLee.java
@@ -1,13 +1,36 @@
 package com.example.rok_lee;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RokLee extends AppCompatActivity {
 
+    private String[] items = {"망고 쥬스", "토마토 쥬스", "포도 쥬스"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rok_lee);
+
+        Button listButton = (Button) findViewById(R.id.button3);
+        listButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RokLee.this);
+                builder.setTitle("리스트");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i){
+                        Toast.makeText(getApplicationContext(), items[i], Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
